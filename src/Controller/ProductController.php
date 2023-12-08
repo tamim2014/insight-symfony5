4b55d2675e36de5136387398bc1d1fa080249c13
unknown <a.andjib@yahoo.fr>
2023-12-08 20:19:27 +0100

    Product: Ajout et affichage dans la même page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\Type\ProductType;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
+//use Services\ProductBundle\Entity\Product; 
+//use Services\ProductBundle\DependencyInjection\Configuration;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class ProductController extends AbstractController
 { 
@@ -19,28 +25,47 @@ class ProductController extends AbstractController
     public function accueil(){
         return $this->render('product/accueil.html.twig');
     }
+
+
+    // On ajoute le produit(par un formulaire)  et on affiche la liste des produits dans la même page
+   
     /**
      * @Route("/product", name="create_product")
      */
-    public function createProduct(EntityManagerInterface $entityManager, ProductRepository $productRepository): Response
+    public function nouveauProduit(EntityManagerInterface $entityManager, Request $request, ProductRepository $productRepository): Response
     {
+        // Instancier un objet Product
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
-        $product->setDescription('Ergonomic and stylish!');
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        //return new Response('Saved new product with id ' . $product->getId());
-        return $this->render('product/show_products.html.twig', [
-            'product' => $product,
+        // Si tu veux(initialiser les champs)
+        $product->setName('PC');
+        $product->setPrice(3000);
+        $product->setDescription('multi-processeurs');
+        // createForm() remplace  createFormBuilder() 
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            $entityManager->persist($product);
+            $entityManager->flush(); // the INSERT query
+            // Redirection (suite au clic sur le bouton save)
+            //return new Response('Tâche prévue: ' . $task->getTask() .'|Tâche numéro: '.$task->getId() );
+        }
+        return $this->renderForm('product/toutdansUnePage.html.twig', [
+            'form' => $form,
+            'produit' => $product,
             'produits' => $productRepository->findBy([])
         ]);
     }
+
+
+
+
+
+
+
+
+
+
 
     /**
      * @Route("/affichetous", name="affichetous")
@@ -82,12 +107,34 @@ class ProductController extends AbstractController
         ]);
     }
     /**
-     * @Route("editinterface", name="edit_interface")
+     * @Route("/editinterfaceP", name="edit_interfaceP")
      */
-    public function editinterface( ProductRepository $productRepository){
-       // $product = $entityManager->getRepository(Product::class)->find($id);
+    //public function editinterface( ProductRepository $productRepository){ // cette signature est suiffisant pour la table sans le formulaire
+    public function editinterface(Request $request, EntityManagerInterface $entityManager, ProductRepository $productRepository){
+        //1. Le formulaire de mise à jour
+        //1.1 Relier le formulaire à l'entité correspondante
+        $product = new Product(); // c faux i fo recuperer, pas instancier, mais pour recuperer il faut un service et je l'ai pas
+        $product->setName('PC');
+        $product->setPrice(3000);
+        $product->setDescription('multi-processeurs');
+        // createForm() remplace  createFormBuilder() 
+        $form = $this->createForm(ProductType::class, $product);
+        //1.2 Requette de saisie (accès en ecriture à la base)
+        $form->handleRequest($request);
+        //1.3 Enregistrement
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            $entityManager->persist($product);
+            $entityManager->flush(); // the INSERT query
+        }
+        //2. La liste des produits
+        // $product = $entityManager->getRepository(Product::class)->find($id);
+        //return $this->render('product/edit_interface.html.twig', [
         return $this->render('product/edit_interface.html.twig', [
+           // 'form' => $form ,
             'produits' => $productRepository->findBy([])
         ]);
     }
+
+
 }
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -18,6 +18,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+
 class TaskController extends AbstractController
 {
     /**
