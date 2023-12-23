f18c0e977a173f025d2bc7bdc962a6885d8df81b
unknown <a.andjib@yahoo.fr>
2023-12-23 16:02:41 +0100

    Toutes les catégories: fantaisie !

---
+++ public/js/touteslescategoriesOC.js
@@ -0,0 +1,21 @@
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("activeOC");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+// open-close
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
